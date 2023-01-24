33c01cbcfae1d35688c7d32c1f850df26c74bb21
Kalaiselvan23 <113838279+Kalaiselvan23@users.noreply.github.com>
2023-01-24 15:08:46 +0530

    crew page is added

--- space_tour/script.js
+++ space_tour/script.js
@@ -12,3 +12,19 @@ function opentab(planet)
         }
     }
 }
+function opencontent(number)
+{
+    const content=document.querySelectorAll(".crew-content");
+    // console.log(content[number-1])
+    // content[number-1].style.display="flex";
+    for(var i=0;i<content.length;i++)
+    {
+        content[i].style.display="none";
+        if(i==number-1)
+        {
+            content[i].style.display="flex";
+        }
+    }
+}
+
+
